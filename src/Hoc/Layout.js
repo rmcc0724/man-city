12dc0b24246a5c1f94e75ab9b5b23bcc4ba2a458
Reid <rmcc0724@gmail.com>
2019-04-24 14:29:15 -0700

    first commit with header

---
+++ src/Hoc/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Components/Header_Footer/Header'
+
+const Layout = (props) => {
+  return (
+    <div>
+        <Header/>
+      {props.children}
+    </div>
+  )
+}
+export default Layout;
